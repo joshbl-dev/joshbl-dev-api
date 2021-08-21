fcbf4c0de2d1d90728d1f9fdabdeed7de2c3ab69
Josh Landsman <github@joshbl.dev>
2021-08-21 03:07:43 -0400

    Update Config.ts

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -26,7 +26,7 @@ export class Config {
 		this.onedriveMainFolderId = process.env.ONEDRIVE_PHOTOS_FOLDER;
 		this.onedriveMemoriesFolderId = process.env.ONEDRIVE_MEMORIES_FOLDER;
 		this.mariaDBHostname = process.env.MARIADB_HOSTNAME;
-		this.mariaDB = process.env.MARIAD;
+		this.mariaDB = process.env.MARIADB;
 		this.mariaDBPort = process.env.MARIADB_PORT;
 		this.mariaDBUsername = process.env.MARIADB_USERNAME;
 		this.mariaDBPassword = process.env.MARIADB_PASSWORD;
