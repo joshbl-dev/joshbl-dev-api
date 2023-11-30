04eb21c92e4bc661aa1bf5d1c82972d51d99e1b7
Josh Landsman <jlandsman@gatech.edu>
2023-11-30 17:47:52 -0500

    Update api.module.ts

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AssetsAPIModule } from "./assets/assetsAPI.module";
 import { AutomationsAPIModule } from "./automations/automationsAPI.module";
+import { HooksAPIModule } from "./hooks/hooksAPI.module";
 
 @Module({
-	imports: [AssetsAPIModule, AutomationsAPIModule]
+	imports: [AssetsAPIModule, AutomationsAPIModule, HooksAPIModule]
 })
 export class ApiModule {}
