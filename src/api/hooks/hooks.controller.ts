c96b9bf1281be208e4d4e1c1d8604cb62a75b8cc
Josh Landsman <jlandsman@gatech.edu>
2023-12-01 13:09:18 -0500

    Update hooks.controller.ts

--- src/api/hooks/hooks.controller.ts
+++ src/api/hooks/hooks.controller.ts
@@ -1,9 +1,9 @@
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
-import { Controller, Logger, Post, UseGuards } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Logger, Post, UseGuards } from "@nestjs/common";
 import { HooksService } from "../../domain/hooks/hooks.service";
 import { GithubGuard } from "../../domain/auth/guards/github.guard";
+import { GithubDto } from "./models/requests/Github.dto";
 
-@ApiBearerAuth("access-token")
 @ApiTags("hooks")
 @Controller("hooks")
 export class HooksController {
@@ -13,7 +13,7 @@ export class HooksController {
 
 	@UseGuards(GithubGuard)
 	@Post("/github")
-	async getGithubData(data) {
+	async getGithubData(@Body() data: GithubDto) {
 		this.logger.log("Received Github hook ");
 		this.logger.log(data);
 
