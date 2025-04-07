2d4aa0a78a0997f4e3975bf285c2654265ad4d20
Josh Landsman <github@joshbl.dev>
2025-04-07 14:24:30 -0400

    etherwake command fix

--- src/domain/automations/automations.service.ts
+++ src/domain/automations/automations.service.ts
@@ -1,18 +1,18 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { exec } from "child_process";
-import { Config } from "../../utils/Config";
-import { AuthService } from "../auth/auth.service";
+import {Injectable, Logger} from "@nestjs/common";
+import {exec} from "child_process";
+import {Config} from "../../utils/Config";
+import {AuthService} from "../auth/auth.service";
 
 @Injectable()
 export class AutomationsService {
-	private readonly logger = new Logger(AutomationsService.name);
+    private readonly logger = new Logger(AutomationsService.name);
 
-	constructor(private config: Config, private authService: AuthService) {
-	}
+    constructor(private config: Config, private authService: AuthService) {
+    }
 
-	startPC() {
-		this.authService.validateAdmin();
-		this.logger.log("Starting PC");
-		exec(`sudo etherwake -i eth0 ${this.config.etherwakeMac}`);
-	}
+    startPC() {
+        this.authService.validateAdmin();
+        this.logger.log("Starting PC");
+        exec(`sudo etherwake ${this.config.etherwakeMac}`);
+    }
 }
